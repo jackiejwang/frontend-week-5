5ffa8537c1608daab8f2362a0032bbb009270ff2
Your Name <you@example.com>
2015-11-16 21:42:13 -0800

    jackie edit 11/16

--- code-appetizer/script.js
+++ code-appetizer/script.js
@@ -1,6 +1,16 @@
-/*
+var images = document.createElement("img");
 
-  Your Javascript here
+img[0].src="http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg";
+img[1].src="http://rufflifechicago.com/wp-content/uploads/cat-treats.jpg";
+img[2].src="http://lovemeow.com/wp-content/uploads/2010/06/cat14.jpg"
 
-*/
+function imgCycle() {
+for (i=0;i<img.length;i++) {
+	if (i=img.length){
+		return img[0];
+	else {return img[i];
+	}
+}
+}
 
+images.addEventListener("click",imgCycle());    
--- exercise-1/script.js
+++ exercise-1/script.js
@@ -1,5 +1,31 @@
-/*
+function addTodoItem(event) {
+  	event.preventDefault();
+  	// necessary to prevent the submitting of a form
+	var inputItem = document.getElementById("inputItem");
+	// console.log(inputItem.value);
+	var toDoList=document.getElementById("toDoList");
+	var listItem = document.createElement("label");
 
-  Your Javascript here
+	// add checkbox here
+	var checkBox =document.createElement("input");
+	checkBox.setAttribute("type","checkbox");
+	listItem.appendChild(checkBox);
+	var text=document.createTextNode(inputItem.value);
+	listItem.appendChild(text);
+	
+	
+	var addBreak = document.createElement("br");
+	listItem.appendChild(addBreak);
 
-*/
+	toDoList.appendChild(listItem);
+
+	inputItem.value="";
+	// inputItem.innerText="";
+}
+
+var form = document.getElementById("form");
+form.addEventListener("submit",addTodoItem);
+
+
+// var appendItem = document.createElement("li");
+// appendItem.innerText = "inputItem";
